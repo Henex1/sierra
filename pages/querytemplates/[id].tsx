45c14909dc8a6cb5743ca58182e69deda09ffd28
Ryan Patterson <ryan@bigdataboutique.com>
2021-04-09 20:14:05 +0800

    Use new apiRequest method on query template pages

--- pages/querytemplates/[id].tsx
+++ pages/querytemplates/[id].tsx
@@ -2,65 +2,56 @@ import * as React from "react";
 import { useRouter } from "next/router";
 
 import { authenticatedPage } from "../../lib/auth";
-import {ExposedQueryTemplate, formatQueryTemplate, userCanAccessQueryTemplate} from "../../lib/querytemplates";
+import {
+  ExposedQueryTemplate,
+  formatQueryTemplate,
+  userCanAccessQueryTemplate,
+} from "../../lib/querytemplates";
 import Container from "@material-ui/core/Container";
 import Form from "../../components/querytemplates/Form";
 import prisma from "../../lib/prisma";
+import { apiRequest } from "../../lib/api";
 
 export const getServerSideProps = authenticatedPage(async (context) => {
-    const template = await prisma.queryTemplate.findFirst({
-        where: userCanAccessQueryTemplate(context.user, {
-            id: parseInt(context.params!.id! as string, 10),
-        }),
-    });
-    if (!template) {
-        return { notFound: true };
-    }
-    return {
-        props: {
-            template: formatQueryTemplate(template)
-        },
-    };
+  const template = await prisma.queryTemplate.findFirst({
+    where: userCanAccessQueryTemplate(context.user, {
+      id: parseInt(context.params!.id! as string, 10),
+    }),
+  });
+  if (!template) {
+    return { notFound: true };
+  }
+  return {
+    props: {
+      template: formatQueryTemplate(template),
+    },
+  };
 });
 
 type Props = {
-    template: ExposedQueryTemplate;
+  template: ExposedQueryTemplate;
 };
 
 export default function EditQueryTemplate({ template }: Props) {
-    const router = useRouter();
+  const router = useRouter();
 
-    async function onSubmit(value: ExposedQueryTemplate) {
-        const response = await fetch(`/api/querytemplates/update`, {
-            method: "POST",
-            body: JSON.stringify({
-                id: template.id,
-                description: value.description,
-                query: value.query,
-                tag: value.tag,
-                projectId: value.projectId,
-                knobs: value.knobs
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        const body = await response.json();
-        if (!response.ok) {
-            // XXX - do something about this
-            throw new Error(JSON.stringify(body));
-        }
-        router.push("/querytemplates");
-        // Keep the form stuck as pending
-        return new Promise(() => {});
-    }
+  async function onSubmit(value: ExposedQueryTemplate) {
+    await apiRequest(`/api/querytemplates/update`, {
+      id: template.id,
+      description: value.description,
+      query: value.query,
+      tag: value.tag,
+      projectId: value.projectId,
+      knobs: value.knobs,
+    });
+    router.push("/querytemplates");
+    // Keep the form stuck as pending
+    return new Promise(() => {});
+  }
 
-    return (
-        <Container maxWidth="sm">
-            <Form
-                onSubmit={onSubmit}
-                initialValues={template}
-            />
-        </Container>
-    );
+  return (
+    <Container maxWidth="sm">
+      <Form onSubmit={onSubmit} initialValues={template} />
+    </Container>
+  );
 }
--- pages/querytemplates/create.tsx
+++ pages/querytemplates/create.tsx
@@ -3,33 +3,23 @@ import { useRouter } from "next/router";
 
 import { ExposedQueryTemplate } from "../../lib/querytemplates";
 import { authenticatedPage } from "../../lib/auth";
+import { apiRequest } from "../../lib/api";
 import Form from "../../components/querytemplates/Form";
 
 export const getServerSideProps = authenticatedPage();
 
 export default function CreateRuleset() {
-    const router = useRouter();
-    async function onSubmit(values: ExposedQueryTemplate) {
-        const response = await fetch(`/api/querytemplates/create`, {
-            method: "POST",
-            body: JSON.stringify(values),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        const body = await response.json();
-        if (!response.ok) {
-            // XXX - do something about this
-            throw new Error(JSON.stringify(body));
-        }
-        router.push("/querytemplates");
-        // Keep the form stuck as pending
-        return new Promise(() => {});
-    }
+  const router = useRouter();
+  async function onSubmit(values: ExposedQueryTemplate) {
+    await apiRequest(`/api/querytemplates/create`, values);
+    router.push("/querytemplates");
+    // Keep the form stuck as pending
+    return new Promise(() => {});
+  }
 
-    return (
-        <Container maxWidth="sm">
-            <Form onSubmit={onSubmit} />
-        </Container>
-    );
+  return (
+    <Container maxWidth="sm">
+      <Form onSubmit={onSubmit} />
+    </Container>
+  );
 }
