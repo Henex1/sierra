720b5361e4f0a69e8aa9ae3896439e7e5bd0a0a8
Lior Friedler <lior@bigdataboutique.com>
2021-04-27 06:02:07 +0000

    #57 - Auto create query template and judgement when creating project

--- lib/querytemplates/index.tsx
+++ lib/querytemplates/index.tsx
@@ -1,30 +1,50 @@
-import {Prisma,QueryTemplate, User} from "../prisma";
-import {userCanAccessProject} from "../projects";
+import prisma, { Prisma, QueryTemplate, User } from "../prisma";
+import { userCanAccessProject } from "../projects";
 import _ from "lodash";
 
 const selectKeys = {
-    id: true,
-    projectId: true,
-    parentId: true,
-    description: true,
-    knobs: true,
-    tag: true,
-    query: true,
+  id: true,
+  projectId: true,
+  parentId: true,
+  description: true,
+  knobs: true,
+  tag: true,
+  query: true,
 };
 
 export type ExposedQueryTemplate = Pick<QueryTemplate, keyof typeof selectKeys>;
 
 export function userCanAccessQueryTemplate(
-    user: User,
-    rest?: Prisma.QueryTemplateWhereInput
+  user: User,
+  rest?: Prisma.QueryTemplateWhereInput
 ): Prisma.QueryTemplateWhereInput {
-    const result: Prisma.QueryTemplateWhereInput = { project: userCanAccessProject(user) };
-    if (rest) {
-        result.AND = rest;
-    }
-    return result;
+  const result: Prisma.QueryTemplateWhereInput = {
+    project: userCanAccessProject(user),
+  };
+  if (rest) {
+    result.AND = rest;
+  }
+  return result;
 }
 
 export function formatQueryTemplate(val: QueryTemplate): ExposedQueryTemplate {
-    return _.pick(val, _.keys(selectKeys)) as ExposedQueryTemplate;
+  return _.pick(val, _.keys(selectKeys)) as ExposedQueryTemplate;
+}
+
+const createKeys = {
+  projectId: true,
+  description: true,
+  knobs: true,
+  tag: true,
+  query: true,
+};
+
+type CreateQueryTemplate = Pick<QueryTemplate, keyof typeof createKeys>;
+
+export async function createQueryTemplate(
+  input: CreateQueryTemplate
+): Promise<QueryTemplate> {
+  return await prisma.queryTemplate.create({
+    data: { ...input },
+  });
 }
