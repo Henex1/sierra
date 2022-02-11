1f65ab00e8180fa1bd1061561804640199f56823
Hayk Mkrtchyan <mhayk2898@gmail.com>
2022-02-11 07:02:00 +0000

    disabled Edit Organization page items for user-scoped org

--- components/AvatarUpload/Input.tsx
+++ components/AvatarUpload/Input.tsx
@@ -11,19 +11,27 @@ const useStyles = makeStyles(() => ({
   input: {
     display: "none",
   },
+  disabledInput: {
+    width: 250,
+    height: 250,
+    display: "block",
+    opacity: 0.3,
+  },
 }));
 
 export interface Props {
   onChange: (file: File) => void;
+  disabled: boolean | undefined;
 }
 
-export const Input: FC<Props> = ({ onChange, children }) => {
+export const Input: FC<Props> = ({ onChange, children, disabled = false }) => {
   const classes = useStyles();
 
   return (
-    <label className={classes.root}>
+    <label className={disabled ? classes.disabledInput : classes.root}>
       {children}
       <input
+        disabled={disabled}
         type="file"
         accept="image/jpeg, image/png, image/svg+xml"
         className={classes.input}
